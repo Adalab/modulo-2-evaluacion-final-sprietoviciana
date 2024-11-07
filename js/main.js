8e88ba8490a6ce7acc63c249ad08667539b9fd53
Silvia Prieto <info@silviaprieto.com>
2024-11-07 13:37:23 +0100

    exercise 2

--- js/main.js
+++ js/main.js
@@ -2,15 +2,50 @@
 
 /* Pasos:
 
--Crear clases.
--Seleccionar los elementos que necesito: input, boton y divs.
+-Crear clases. HECHO
+-Seleccionar los elementos que necesito: input, boton y divs. HECHO
 - Cuando haga click en el boton de buscar:
-   - recojo el valor del input
-   - se hace una llamada al servidor con los datos del api.
-   - si coincide con alguna descripcion de alguna serie, que se pinte en pantalla.
-   - para que salgan las series que contengan el valor del input, tengo que concatenar el input en la url del api.
-   - tiene que mostrarse la imagen y la descripcion de la serie.
+   - recojo el valor del input. HECHO
+   - se hace una llamada al servidor con los datos del api. HECHO
+   - si coincide con alguna descripcion de alguna serie, que se pinte en pantalla. HECHO
+   - para que salgan las series que contengan el valor del input, tengo que concatenar el input en la url del api. HECHO
+   - tiene que mostrarse la imagen y la descripcion de la serie. HECHO
    - si la serie no tiene img, meter nuestra propia imagen de relleno. 
      Hay que hacerlo de esta forma: https://via.placeholder.com/210x295/ffffff/666666/?text=TV
 */
- 
+
+const input = document.querySelector(".js-input");
+const buttonSearch = document.querySelector(".js-btn-search");
+const containers = document.querySelector(".containers");
+
+containers.inde;
+let animeList = [];
+let animeFavorities = [];
+
+function handleclick() {
+  const inputValue = input.value;
+
+  fetch(`https://api.jikan.moe/v4/anime?q=${inputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const animes = data.data;
+      containers.innerHTML = "";
+      for (const anime of animes) {
+        console.log(anime);
+        let url = anime.images.jpg.image_url;
+        if (
+          url ===
+          "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
+        ) {
+          url = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV.";
+        } else {
+          containers.innerHTML += `<div class="list-anime">
+        <h5>${anime.title}</h5>
+        <img src="${url}">
+        </div>`;
+        }
+      }
+    });
+}
+
+buttonSearch.addEventListener("click", handleclick);
