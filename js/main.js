00dfafc4eb11880f7ce72c6901ae640d276f20a1
Silvia Prieto <info@silviaprieto.com>
2024-11-07 20:48:28 +0100

    add favorites in LS

--- js/main.js
+++ js/main.js
@@ -1,31 +1,17 @@
 "use strict";
 
-/* Pasos:
-
--Crear clases. HECHO
--Seleccionar los elementos que necesito: input, boton y divs. HECHO
-- Cuando haga click en el boton de buscar:
-   - recojo el valor del input. HECHO
-   - se hace una llamada al servidor con los datos del api. HECHO
-   - si coincide con alguna descripcion de alguna serie, que se pinte en pantalla. HECHO
-   - para que salgan las series que contengan el valor del input, tengo que concatenar el input en la url del api. HECHO
-   - tiene que mostrarse la imagen y la descripcion de la serie. HECHO
-   - si la serie no tiene img, meter nuestra propia imagen de relleno. HECHO
-     Hay que hacerlo de esta forma: https://via.placeholder.com/210x295/ffffff/666666/?text=TV
-
-LISTADO DE FAVORITOS
-- Cuando un usuario le de click encima de una pelicula, se añadirá a favoritos.
-- se irá incluyendo en la lista vacia.
-- tambien se cambiará el color de fondo en ambas partes y el titulo tambien cambiará de color, para que destaque.
-- no se deben quitar las peliculas de la lista de favoritos cuando se haga otra busqueda.
-- guardar la lista de favoritos en LS.
-*/
-
 const input = document.querySelector(".js-input");
 const buttonSearch = document.querySelector(".js-btn-search");
 const containers = document.querySelector(".js-containers");
 const favoritesContainer = document.querySelector(".js-favorities");
+
+const favoritesFromLocalStorage = localStorage.getItem("filmFavorites");
 let favorites = [];
+
+if (favoritesFromLocalStorage !== null) {
+  favorites = JSON.parse(favoritesFromLocalStorage);
+  renderFavorites();
+}
 
 function renderFavorites() {
   favoritesContainer.innerHTML = "";
@@ -64,6 +50,7 @@ function handleclick() {
             title: anime.title,
             image: url,
           });
+          localStorage.setItem("filmFavorites", JSON.stringify(favorites));
           renderFavorites();
         });
 
