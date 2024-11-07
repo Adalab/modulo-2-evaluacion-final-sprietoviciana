02a93da1e83c7a32c3725d46f304585290bbe683
Silvia Prieto <info@silviaprieto.com>
2024-11-07 09:52:00 +0100

    class and steps

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+/* Pasos:
+
+-Crear clases.
+-Seleccionar los elementos que necesito: input, boton y divs.
+- Cuando haga click en el boton de buscar:
+   - recojo el valor del input
+   - se hace una llamada al servidor con los datos del api.
+   - si coincide con alguna descripcion de alguna serie, que se pinte en pantalla.
+   - para que salgan las series que contengan el valor del input, tengo que concatenar el input en la url del api.
+   - tiene que mostrarse la imagen y la descripcion de la serie.
+   - si la serie no tiene img, meter nuestra propia imagen de relleno. 
+     Hay que hacerlo de esta forma: https://via.placeholder.com/210x295/ffffff/666666/?text=TV
+*/
+ 
